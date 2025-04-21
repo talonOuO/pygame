52f91b16691688d1623802c7bfd800b37789a093
talonOuO <poiuytrewq0201p@gmail.com>
2025-04-21 18:50:38 +0800

    建立遊戲框架2

--- main/main.py
+++ main/main.py
@@ -7,3 +7,15 @@ screenHigh = 760
 screenWidth = 1000
 playground = [screenWidth,screenHigh]
 screen = pygame.display.set_mode((screenWidth,screenHigh))
+
+running = True
+fps = 120
+clock =pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type ==   pygame.QUIT:
+            running=False
+    pygame.display.update()
+    dt =clock.tick(fps)
+pygame.quit()
