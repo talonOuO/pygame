93847b02ad4f8ca2cc37e2ca6f3b574b2c8e5379
talonOuO <poiuytrewq0201p@gmail.com>
2025-04-21 18:49:35 +0800

    建立遊戲框架1

---
+++ main/main.py
@@ -0,0 +1,9 @@
+import pygame
+from pexpect.screen import screen
+
+pygame.init()
+
+screenHigh = 760
+screenWidth = 1000
+playground = [screenWidth,screenHigh]
+screen = pygame.display.set_mode((screenWidth,screenHigh))
